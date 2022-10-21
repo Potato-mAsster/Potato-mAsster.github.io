68ca438b1a1f2a54f5cc5334be67e21a772349a8
Potato-mAsster <112930084+Potato-mAsster@users.noreply.github.com>
2022-10-21 11:48:17 +0600

    Add files via upload

---
+++ сайт/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+document.documentElement.style.setProperty('--animate-duration', '2s');
+
+// All animations will take half the time to accomplish
+document.documentElement.style.setProperty('--animate-duration', '.5s');
